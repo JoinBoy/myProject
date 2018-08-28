e8e6c17cd0e8408db04d78d5b72dc69640530733
Join <364229070@qq.com>
2018-08-28 12:56:02 +0800

    更改配置文件及jar包

---
+++ myProject/src/main/java/com/ccj/vueProject/index/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.ccj.vueProject.index.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public String index(HttpServletRequest request, HttpServletResponse response){
+
+		return "index";
+	}
+}
